779c8048151f7d5f676e766f25c9e07dfad612de
Kasper Sapala <kaspersapala@MacBook-Pro-Kasper.local>
2018-01-27 16:33:07 +0100

    check

--- AppTime/settings/__init__.py
+++ AppTime/settings/__init__.py
@@ -4,6 +4,8 @@ from .base_settings import *
 local_hosts = ['MacBook-Pro-Kasper.local']
 
 if socket.gethostname() in local_hosts:
+    print("local")
     from .local_settings import *
 else:
+    print("production")
     from .production_settings import *
